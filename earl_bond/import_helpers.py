babf93f07bbfeb15c79450d5c1430d45a264c406
Rasool Somji <rasoolsomji@Rasools-MacBook-Pro.local>
2015-08-27 13:41:29 +0100

    MOAR

---
+++ earl_bond/import_helpers.py
@@ -0,0 +1,29 @@
+import re
+
+def import_urls_from_text_file(filename, skip_header_row=False):
+	
+	urls = []
+
+	with open(filename, 'r') as input_file:
+
+		if skip_header_row:
+			next(input_file)
+
+		for raw_line in input_file:
+
+			if not raw_line.isspace():
+				cleaned_line = raw_line.replace("\n", '')
+				#cleaned_line = cleaned_line.replace('\x00', '')
+
+				wrapped_in_quotes_matcher = re.search('^"(.*)"$', cleaned_line)
+
+				if wrapped_in_quotes_matcher:
+					if not wrapped_in_quotes_matcher.group(1).isspace():
+						cleaned_line = wrapped_in_quotes_matcher.group(1)
+						urls.append(cleaned_line)
+
+				else:
+					urls.append(cleaned_line)
+
+	return urls
+
--- earl_bond/main.py
+++ earl_bond/main.py
@@ -1,12 +1,41 @@
-from helper import import_urls_from_text_file, dedupe
+from import_helpers import import_urls_from_text_file
+from helper import dedupe
 from clean_urls import clean_and_strip
-from parse_urls import list_of_component
+from parse_urls import list_of_component, sub_plus_registered_domain
 
-def domains_from_file(text_file, domain='registered_domain', dupes=True):
+def individual_components_from_file(text_file, component='registered_domain', dupes=True):
 	urls = import_urls_from_text_file(text_file)
 	urls = clean_and_strip(urls)
-	domains = list_of_component(urls, component=domain)
+	domains = list_of_component(urls, component=component)
 	if not dupes:
 		return dedupe(domains)
 	return domains
 
+
+def sub_plus_reg_from_file(text_file, dupes=True):
+	urls = import_urls_from_text_file(text_file)
+	urls = clean_and_strip(urls)
+	domains = sub_plus_registered_domain(urls)
+	if not dupes:
+		return dedupe(domains)
+	return domains
+
+
+def apply_domain_limit(urls, domain_limit=3):
+	new_url_set = set()
+	domains_seen = set()
+	domains_count = dict()
+	for url in urls:
+		if url not in domains_seen:
+			domains_seen.add(url)
+			domains_count[url] = 1
+		else:
+			domains_count[url] += 1
+
+	for url in urls:
+		if domains_count[url] >= domain_limit:
+			new_url_set.add('domain: ' + url)
+		else:
+			new_url_set.add(url)
+
+	return list(new_url_set)
