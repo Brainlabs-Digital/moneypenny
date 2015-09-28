490a154c326f586d0c6d94f617c57a538477fbe2
Tom Anthony <git@tomanthony.co.uk>
2015-09-28 11:51:54 +0100

    Bump version number -> 0.0.3.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='moneypenny',
     packages=['moneypenny'],
-    version='0.0.2',
-    description='A library for normalizing URL lists and creating disavow files.',
+    version='0.0.3',
+    description='A library for normalizing URL lists and creating Google disavow files.',
     author='Rasool Somji',
     author_email='rasool.somji@distilled.net',
     url='https://github.com/DistilledLtd/moneypenny',
-    download_url='https://github.com/DistilledLtd/moneypenny/tarball/0.0.2',
+    download_url='https://github.com/DistilledLtd/moneypenny/tarball/0.0.3',
     keywords=['urls', 'disavow'],
     classifiers=[],
     install_requires=[
